127b1336a7d860244d77649a5ab89ea0bdd2fd4f
Jérôme Rastit <jerome.rastit@libertysurf.fr>
2021-10-24 18:18:03 +0200

    fix sgx

--- script/admin/install_sgx.sh
+++ script/admin/install_sgx.sh
@@ -12,13 +12,17 @@ $REMOTE_CMD_ADMIN "curl -sSL \"https://download.fortanix.com/linux/apt/fortanix.
 $REMOTE_CMD_ADMIN "echo \"deb https://download.01.org/intel-sgx/sgx_repo/ubuntu \$(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/intel-sgx.list >/dev/null"
 $REMOTE_CMD_ADMIN "curl -sSL \"https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key\" | sudo -E apt-key add -"
 
+
 echo $REMOTE_CMD_ADMIN apt-get update
 $REMOTE_CMD_ADMIN apt-get update
 
 echo $REMOTE_CMD_ADMIN apt-get install bubblewrap intel-sgx-dkms sgx-aesm-service libsgx-aesm-launch-plugin libsgx-aesm-epid-plugin
 $REMOTE_CMD_ADMIN apt-get install bubblewrap intel-sgx-dkms sgx-aesm-service libsgx-aesm-launch-plugin libsgx-aesm-epid-plugin
 
-$REMOTE_CMD_ADMIN systemctl status aesmd.service
+echo fix SGX /dev issue => $REMOTE_CMD_ADMIN "sed -i s/devsgx=y/devsgx=n/g /etc/modprobe.d/fortanix-isgx.conf"
+$REMOTE_CMD_ADMIN sed -i s/devsgx=y/devsgx=n/g /etc/modprobe.d/fortanix-isgx.conf
+
+
 
 
 
--- script/install/check_paratime_sgx.sh
+++ script/install/check_paratime_sgx.sh
@@ -2,5 +2,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 . $SCRIPT_DIR/oasis_env.sh
 
+echo check isgx device
+ls -l /dev/isgx
+
+echo check aesmd status
+$REMOTE_CMD_ADMIN systemctl status aesmd.service
+
 echo nmap $CUSTOM_OASIS_NODE_ADDR -p $OASIS_NODE_PORT,$CUSTOM_PARATIME_WORKER_CLIENT_PORT,$CUSTOM_PARATIME_WORKER_P2P_PORT -Pn
 nmap $CUSTOM_OASIS_NODE_ADDR -p $OASIS_NODE_PORT,$CUSTOM_PARATIME_WORKER_CLIENT_PORT,$CUSTOM_PARATIME_WORKER_P2P_PORT -Pn
