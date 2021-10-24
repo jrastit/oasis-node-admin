e76807b49d0a84a2d1da4767701279b77226d057
jrastit <jerome.rastit@libertysurf.fr>
2021-10-24 16:58:42 +0200

    add remote sheel

--- script/node_admin/copy_from_node.sh
+++ script/node_admin/copy_from_node.sh
@@ -6,6 +6,6 @@ echo source node data directory :
 read SOURCE_NODE_DATA_DIR
 
 $REMOTE_CMD rm -rf $NETWORK_DIR/node/data/tendermint/oasis_priv_validator.json $NETWORK_DIR/node/data/tendermint/config
-$REMOTE_CMD rsync -av --stats --progress $SOURCE_NODE_DATA_DIR/tendermint/data $SOURCE_NODE_DATA_DIR/tendermint/abci-state $NETWORK_DIR/node/data/tendermint
+$REMOTE_CMD rsync -av --stats --progress $SOURCE_NODE_DATA_DIR/tendermint/data $SOURCE_NODE_DATA_DIR/tendermint/abci-state $NETWORK_DIR/node/data/tendermint 
 $REMOTE_CMD chmod go-xrw -R $NETWORK_DIR/node/data/tendermint
 
