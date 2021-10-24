4989f876ab521e264e3e186a875e933e0253674f
jrastit <jerome.rastit@libertysurf.fr>
2021-10-24 15:37:49 +0200

    quick fix

--- script/transfer/transfer.sh
+++ script/transfer/transfer.sh
@@ -16,7 +16,7 @@ $SCRIPT_DIR/oasis_local.sh stake account gen_transfer \
 	--signer.backend file \
 	--signer.dir $ENTITY_DIR \
 	--stake.amount $AMOUNT \
-	--stake.transfer.destination ADDRESS \
+	--stake.transfer.destination $ADDRESS \
 	--transaction.file $OUTPUT_TX \
 	--transaction.fee.gas 1500 \
 	--transaction.fee.amount 0 \
