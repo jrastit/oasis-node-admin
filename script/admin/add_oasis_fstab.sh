c1ebdcad430a31fbfc5af8bf922c31f3e91a33be
Jérôme Rastit <jerome.rastit@libertysurf.fr>
2021-10-25 15:41:22 +0200

    add fstab

--- script/admin/add_oasis_fstab.sh
+++ script/admin/add_oasis_fstab.sh
@@ -6,5 +6,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 echo "oasis disk UUID"
 read OASIS_UUID
 
+$REMOTE_CMD_ADMIN mkdir -p /oasis/
+
 echo $REMOTE_CMD_ADMIN "\"echo 'UUID=$OASIS_UUID /oasis auto defaults,nofail,x-systemd.requires=cloud-init.service,comment=cloudconfig 0 2' >> /etc/fstab\""
 $REMOTE_CMD_ADMIN "bash -c \"echo 'UUID=$OASIS_UUID /oasis auto defaults,nofail,x-systemd.requires=cloud-init.service,comment=cloudconfig 0 2' >> /etc/fstab\""
+
+$REMOTE_CMD_ADMIN mount -a
+
+$REMOTE_CMD_ADMIN mkdir /oasis/$NETWORK_NODE_NAME
+$REMOTE_CMD_ADMIN chown oasis.oasis /oasis/$NETWORK_NODE_NAME
+
