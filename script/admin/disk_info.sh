03f0a9f9c2e57644d13ff05fb79dbbec204cbc43
Jérôme Rastit <jerome.rastit@libertysurf.fr>
2021-10-25 17:05:37 +0200

    fix fstab

--- script/admin/disk_info.sh
+++ script/admin/disk_info.sh
@@ -3,7 +3,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 . $SCRIPT_DIR/oasis_env.sh
 
 
-$REMOTE_CMD_ADMIN lsblk
-$REMOTE_CMD_ADMIN ls -lrt /dev/disk/by-uuid
-$REMOTE_CMD_ADMIN cat /etc/fstab
+$REMOTE_CMD_ADMIN bash -c "partprobe "
+$REMOTE_CMD_ADMIN bash -c "lsblk && ls -lrt /dev/disk/by-uuid && cat /etc/fstab"
 
