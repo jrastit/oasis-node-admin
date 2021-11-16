45eedb60183c763d2685de76ac2e53d9e3718974
jrastit <jerome.rastit@libertysurf.fr>
2021-11-17 00:20:30 +0100

    add mv from node

---
+++ script/node_admin/mv_from_node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
+. $SCRIPT_DIR/oasis_env.sh
+
+echo source node data directory : 
+read SOURCE_NODE_DATA_DIR
+
+$REMOTE_CMD mkdir -p  $OASIS_NODE_DIR/node/data/tendermint
+$REMOTE_CMD rm -rf $OASIS_NODE_DIR/node/data/tendermint/oasis_priv_validator.json $OASIS_NODE_DIR/node/data/tendermint/config
+$REMOTE_CMD mv $SOURCE_NODE_DATA_DIR/tendermint/data $SOURCE_NODE_DATA_DIR/tendermint/abci-state $OASIS_NODE_DIR/node/data/tendermint 
+$REMOTE_CMD chmod go-xrw -R $OASIS_NODE_DIR/node/data/tendermint
+
