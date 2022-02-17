e041c181b164936a2cfccb7624f986a16c4a2d58
jrastit <jerome.rastit@libertysurf.fr>
2022-02-17 22:05:12 +0100

    update

--- script/admin/add_oasis_user.sh
+++ script/admin/add_oasis_user.sh
@@ -2,8 +2,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 . $SCRIPT_DIR/oasis_env.sh
 
-$REMOTE_CMD_ADMIN adduser oasis --home /mnt/oasis 
-$REMOTE_CMD_ADMIN ln -s /mnt/oasis /home
+$REMOTE_CMD_ADMIN adduser oasis --home /home/oasis 
+#$REMOTE_CMD_ADMIN ln -s /home/oasis /home
 
 if [[ -n "$OASIS_NODE_SSH_ID" ]]
 then
@@ -15,9 +15,9 @@ fi
 
 if [[ -n "$OASIS_NODE_SSH_ID" ]]
 then
-	$REMOTE_CMD_ADMIN "mkdir /mnt/oasis/.ssh"
-	$REMOTE_CMD_ADMIN "chown oasis.oasis /mnt/oasis/.ssh"
-	$REMOTE_CMD_ADMIN "echo \"$OASIS_NODE_SSH_ID\" | sudo tee /mnt/oasis/.ssh/authorized_keys"
-	$REMOTE_CMD_ADMIN "chown oasis.oasis /mnt/oasis/.ssh/authorized_keys"
+	$REMOTE_CMD_ADMIN "mkdir /home/oasis/.ssh"
+	$REMOTE_CMD_ADMIN "chown oasis.oasis /home/oasis/.ssh"
+	$REMOTE_CMD_ADMIN "echo \"$OASIS_NODE_SSH_ID\" | sudo tee /home/oasis/.ssh/authorized_keys"
+	$REMOTE_CMD_ADMIN "chown oasis.oasis /home/oasis/.ssh/authorized_keys"
 fi
 
---
+++ script/node_admin/update_cron_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
+. $SCRIPT_DIR/oasis_env.sh
+
+STAKE_ADDRESS=`$SCRIPT_ENTITY_INFO_DIR/get_entity_address.sh`
+$REMOTE_CMD mkdir -p $OASIS_NODE_DIR/cron/script
+$REMOTE_CMD mkdir -p $OASIS_NODE_DIR/cron/update_balance
+
+$REMOTE_CMD "cat > $OASIS_NODE_DIR/cron/script/update_balance.sh <<- EOF
+$OASIS_NODE_BIN_PATH/$OASIS_CORE_DIR/oasis-node -a $OASIS_NODE_SOCK --stake.account.address $STAKE_ADDRESS stake account info 2>&1 > $OASIS_NODE_DIR/cron/update_balance/\\\`date +%s\\\`_result.txt
+EOF
+"
+
