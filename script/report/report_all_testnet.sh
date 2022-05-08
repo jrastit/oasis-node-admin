3a8b2b4901b503d778aaa76320d460f3822e6ec1
jrastit <jerome.rastit@libertysurf.fr>
2022-05-08 21:03:42 +0200

    fix bouble version bug

---
+++ script/report/report_all_testnet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
+
+REPORT_CONFIG_DIR=$SCRIPT_DIR/../config
+
+for NODE_DIR in $REPORT_CONFIG_DIR/node/*; do
+	if [ -d $NODE_DIR ]; then
+		OASIS_NODE_NAME=`basename $NODE_DIR`
+		. $SCRIPT_DIR/oasis_env.sh
+		echo NODE $OASIS_NODE_NAME
+		echo NETWORK $OASIS_NODE_NETWORK
+		if [[ $OASIS_NODE_NETWORK == 'testnet' ]]; then
+			$SCRIPT_DIR/$1
+		fi
+		echo
+	fi
+done
+
+
