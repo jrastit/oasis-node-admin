f80dac5318ea8be82ac5edf79d09c1e25ac2976a
jrastit <jerome.rastit@libertysurf.fr>
2023-10-27 17:00:03 +0000

    Add install node script

---
+++ script/install/install_node_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
+. $SCRIPT_DIR/oasis_env.sh
+
+$REMOTE_CMD "cat > $OASIS_NODE_DIR/start_node.sh <<- EOF
+#!/bin/bash
+$OASIS_NODE_BIN_PATH/$OASIS_CORE_DIR/oasis-node --config $OASIS_NODE_CONFIG
+
+EOF"
+
+$REMOTE_CMD "chmod +x $OASIS_NODE_DIR/start_node.sh"
+echo "Node start script created at $OASIS_NODE_DIR/start_node.sh with $OASIS_CORE_DIR"
+
