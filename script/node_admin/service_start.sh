a0b674357ab137bd0988ab12cda9bf80f8309408
jrastit <jerome.rastit@libertysurf.fr>
2023-10-28 11:53:10 +0200

    Fix service start

--- script/node_admin/service_start.sh
+++ script/node_admin/service_start.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 . $SCRIPT_DIR/oasis_env.sh
 
-$REMOTE_CMD systemctl --user daemon-reload
+$REMOTE_CMD systemctl --user daemon-reload && sudo loginctl enable-linger
 $REMOTE_CMD systemctl --user start oasis_$OASIS_NODE_NAME.service
 
 
