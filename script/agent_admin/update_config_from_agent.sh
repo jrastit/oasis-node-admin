3990a01ca90c7e567db3155b1ef7e214c52da011
jrastit <jerome.rastit@libertysurf.fr>
2024-12-07 23:15:50 +0000

    update config from agent

--- script/agent_admin/update_config_from_agent.sh
+++ script/agent_admin/update_config_from_agent.sh
@@ -10,12 +10,12 @@ for NETWORK_DIR in $LOCAL_CONFIG_DIR/network/*; do
 	if [ -d $NETWORK_DIR ]; then
 		NETWORK_DIR_NAME=`basename $NETWORK_DIR`
 		NETWORK_DIR_REMOTE=$REMOTE_CONFIG_DIR/network/$NETWORK_DIR_NAME
-		log_cmd $REMOTE_SYNC $OASIS_NODE_SSH:$NETWORK_DIR_REMOTE $NETWORK_DIR/network_config.sh
+		log_cmd $REMOTE_SYNC $OASIS_NODE_SSH:$NETWORK_DIR_REMOTE/network_config.sh $NETWORK_DIR
 		for TYPE_DIR in $NETWORK_DIR/*; do
 			if [ -d $TYPE_DIR ]; then
 				TYPE_DIR_NAME=`basename $TYPE_DIR`
 				TYPE_DIR_REMOTE=$REMOTE_CONFIG_DIR/network/$NETWORK_DIR_NAME/$TYPE_DIR_NAME
-				log_cmd $REMOTE_SYNC $OASIS_NODE_SSH:$TYPE_DIR_REMOTE $TYPE_DIR/type_config.sh
+				log_cmd $REMOTE_SYNC $OASIS_NODE_SSH:$TYPE_DIR_REMOTE/type_config.sh $TYPE_DIR
 			fi
 		done
 	fi
@@ -25,7 +25,7 @@ for NODE_DIR in $LOCAL_CONFIG_DIR/node/*; do
 	if [ -d $NODE_DIR ]; then
 		NODE_DIR_NAME=`basename $NODE_DIR`
 		NODE_DIR_REMOTE=$REMOTE_CONFIG_DIR/node/$NODE_DIR_NAME
-		log_cmd $REMOTE_SYNC $OASIS_NODE_SSH:$NODE_DIR_REMOTE $NODE_DIR/node_config.sh
+		log_cmd $REMOTE_SYNC $OASIS_NODE_SSH:$NODE_DIR_REMOTE/node_config.sh $NODE_DIR
 	fi
 done
 
