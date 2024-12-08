91df6dd832a12e3c3a61b0e62a815947ac94cc66
jrastit <jerome.rastit@libertysurf.fr>
2024-12-08 10:40:47 +0100

    fix nonce

--- script/entity_info/nonce.sh
+++ script/entity_info/nonce.sh
@@ -2,9 +2,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 . $SCRIPT_DIR/oasis_env.sh
 # NONCE=`$SCRIPT_DIR/myoasis.sh stake account nonce`
-#JSON=`$SCRIPT_DIR/mycli_local.sh account show --network $OASIS_NODE_NETWORK | sed "s/=== CONSENSUS LAYER ($OASIS_NODE_NETWORK) ===/$OASIS_NODE_NETWORK:/" | sed "s/=== sapphire PARATIME ===/sapphire:/" | sed "s/%//" | sed "s/(1)/   /" | sed "s/(2)/   /" | yq`
-#NONCE=`echo -e "$JSON" | jq ".$OASIS_NODE_NETWORK.Nonce"`
-#echo "$NONCE"
-echo "97"
+# JSON=`$SCRIPT_DIR/mycli_local.sh account show --network $OASIS_NODE_NETWORK | sed "s/=== CONSENSUS LAYER ($OASIS_NODE_NETWORK) ===/$OASIS_NODE_NETWORK:/" | sed "s/=== sapphire PARATIME ===/sapphire:/" | sed "s/%//" | sed "s/(1)/   /" | sed "s/(2)/   /" | yq`
+# NONCE=`echo -e "$JSON" | jq ".$OASIS_NODE_NETWORK.Nonce"`
+NONCE=`./mycli_local.sh account show --no-paratime | grep "Nonce:"`
+NONCE=`echo $NONCE | awk '{print $2}'`
+echo "$NONCE"
+#echo "97"
 
 sleep 1
