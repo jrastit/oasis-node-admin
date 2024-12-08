81556d4b92b177cbf89500c106f23b5d51894f58
jrastit <jerome.rastit@libertysurf.fr>
2024-12-08 16:14:48 +0000

    add disable service

--- script/node_admin/service_start.sh
+++ script/node_admin/service_start.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 . $SCRIPT_DIR/oasis_env.sh
 
+$REMOTE_CMD rm $OASIS_NODE_DIR/service_disable 2>/dev/null || true
 $REMOTE_CMD systemctl --user daemon-reload 
 $REMOTE_CMD loginctl enable-linger
 $REMOTE_CMD systemctl --user start oasis_$OASIS_NODE_NAME.service
